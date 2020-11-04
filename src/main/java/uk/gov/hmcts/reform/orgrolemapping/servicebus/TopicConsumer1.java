1252cb245a00b36c695e114c7af25be58e2a6648
Nitish Singla <nitish.singla@hmcts.net>
2020-11-04 11:35:24 +0000

    Adding more classes for ASB

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/servicebus/TopicConsumer1.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/servicebus/TopicConsumer1.java
@@ -20,20 +20,20 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Slf4j
 public class TopicConsumer1 {
 
     static final Gson GSON = new Gson();
-    static final int maxRetries = 3;
+    static final int MAX_RETRIES = 3;
 
     public static void main(String[] args) throws Exception {
         String connectionString = "Endpoint=sb://rd-servicebus-sandbox.servicebus.windows.net/;SharedAccessKeyName=SendAndListenSharedAccessKey;SharedAccessKey=97E6uvE6xHcqHAVlxufN1PH75tMHoZUe78FhsCbLLLQ=";
         SubscriptionClient subscription1Client = new SubscriptionClient(new ConnectionStringBuilder(connectionString, "rd-caseworker-topic-sandbox/subscriptions/temporary"), ReceiveMode.PEEKLOCK);
-        //SubscriptionClient subscription2Client = new SubscriptionClient(new ConnectionStringBuilder(connectionString, "rd-caseworker-topic-sandbox/subscriptions/temporary"), ReceiveMode.PEEKLOCK);
         registerMessageHandlerOnClient(subscription1Client);
-        //registerMessageHandlerOnClient(subscription2Client);
-
         log.info("clients registered.....");
     }
 
@@ -45,17 +45,13 @@ public class TopicConsumer1 {
             // callback invoked when the message handler loop has obtained a message
             @SneakyThrows
             public CompletableFuture<Void> onMessageAsync(IMessage message) {
-                int retryCount = 0;
                 log.info("onMessageAsync.....{}", message);
                 List<byte[]> body = message.getMessageBody().getBinaryData();
                 log.info("body.....{}", body);
                 log.info("token.....{}", message.getLockToken());
                 Integer users = null;
                 try {
-
-                    //int maxRetries = 3;
-                    //users = waitForRas(mapper, body, maxRetries);
-                    for (int i = 0; i < maxRetries; i++) {
+                    for (int i = 0; i < MAX_RETRIES; i++) {
                         log.info("Iteration number :" + i);
                         if (roleAssignmentHealthCheck()) {
                             log.info("Parsing the value.");
@@ -64,19 +60,19 @@ public class TopicConsumer1 {
                             break;
                         }
                     }
-
                 } catch (IOException e) {
                     try {
                         log.info("Abandoned message:" + message.getLockToken());
                         receiveClient.abandon(message.getLockToken());
                     } catch (InterruptedException | ServiceBusException ex) {
                         ex.printStackTrace();
+                        throw ex;
                     }
                     log.error("throwing exception for unprocessable message");
                     throw e;
                 }
                 if (users != null) {
-                    System.out.printf(
+                    log.info(
                             "\n\t\t\t\t%s Message received: \n\t\t\t\t\t\tMessageId = %s, \n\t\t\t\t\t\tSequenceNumber = %s, \n\t\t\t\t\t\tEnqueuedTimeUtc = %s," +
                                     "\n\t\t\t\t\t\tExpiresAtUtc = %s, \n\t\t\t\t\t\tContentType = \"%s\",  \n\t\t\t\t\t\tContent: [ User Id = %s]\n",
                             receiveClient.getEntityPath(),
@@ -87,26 +83,22 @@ public class TopicConsumer1 {
                             message.getContentType(),
                             "",
                             "");
-                }
-                else{
+                } else {
                     log.info("Users is NULL");
                 }
-
-                //System.out.printf("Message consumed successfully..... ");
-                //log.info("token before marking as complete.....{}", message.getLockToken());
                 return receiveClient.completeAsync(message.getLockToken());
             }
 
             public void notifyException(Throwable throwable, ExceptionPhase exceptionPhase) {
                 log.error("Exception occured.");
-                System.out.printf(exceptionPhase + "-" + throwable.getMessage());
+                log.error(exceptionPhase + "-" + throwable.getMessage());
             }
         };
 
         ExecutorService executorService = Executors.newFixedThreadPool(1);
         receiveClient.registerMessageHandler(
                 messageHandler, new MessageHandlerOptions(1,
-                        false, Duration.ofSeconds(30), Duration.ofMinutes(5)),
+                        false, Duration.ofSeconds(90), Duration.ofMinutes(5)),
                 executorService);
 
     }
@@ -115,12 +107,21 @@ public class TopicConsumer1 {
         // Call Health check
         log.info("Calling the health check");
         double var = Math.random();
-        if (var > 0.99) {
+        if (var > 0.50) {
             log.info("Sleeping for 2 seconds");
             Thread.sleep(2000);
             return false;
         }
-        return false;
+        return true;
+    }
+
+    public static void setLevel(Level targetLevel) {
+        Logger root = Logger.getLogger("");
+        root.setLevel(targetLevel);
+        for (Handler handler : root.getHandlers()) {
+            handler.setLevel(targetLevel);
+        }
+        log.info("level set: " + targetLevel.getName());
     }
 
 }
