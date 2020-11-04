6d2fb94e4915f8929280d3456e1ec5b8ce1080cc
Nitish Singla <nitish.singla@hmcts.net>
2020-11-04 12:13:59 +0000

    Adding code for DLQ.

--- src/main/java/uk/gov/hmcts/reform/orgrolemapping/servicebus/RetrieveDeadLetterTopic.java
+++ src/main/java/uk/gov/hmcts/reform/orgrolemapping/servicebus/RetrieveDeadLetterTopic.java
@@ -3,9 +3,11 @@ package uk.gov.hmcts.reform.orgrolemapping.servicebus;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.gson.Gson;
+import com.microsoft.azure.servicebus.ClientFactory;
 import com.microsoft.azure.servicebus.ExceptionPhase;
 import com.microsoft.azure.servicebus.IMessage;
 import com.microsoft.azure.servicebus.IMessageHandler;
+import com.microsoft.azure.servicebus.IMessageReceiver;
 import com.microsoft.azure.servicebus.MessageHandlerOptions;
 import com.microsoft.azure.servicebus.ReceiveMode;
 import com.microsoft.azure.servicebus.SubscriptionClient;
@@ -16,6 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
@@ -27,10 +30,7 @@ public class RetrieveDeadLetterTopic {
     static final Gson GSON = new Gson();
 
     public static void main(String[] args) throws Exception {
-        String connectionString = "Endpoint=sb://rd-servicebus-sandbox.servicebus.windows.net/;SharedAccessKeyName=SendAndListenSharedAccessKey;SharedAccessKey=97E6uvE6xHcqHAVlxufN1PH75tMHoZUe78FhsCbLLLQ=";
-        SubscriptionClient subscription1Client = new SubscriptionClient(new ConnectionStringBuilder(connectionString, "rd-caseworker-topic-sandbox/subscriptions/rd-caseworker-subscription-sandbox"), ReceiveMode.PEEKLOCK);
-        registerMessageHandlerOnClient(subscription1Client);
-
+        deleteMessagesInEnvelopesDlq();
         log.info("clients registered.....");
     }
 
@@ -84,5 +84,71 @@ public class RetrieveDeadLetterTopic {
 
     }
 
+    //@Scheduled(cron = "${scheduling.task.delete-envelopes-dlq-messages.cron}")
+    public static void deleteMessagesInEnvelopesDlq() throws ServiceBusException, InterruptedException {
+        log.info("Started {} job", "deleteMessagesInEnvelopesDlq");
+        IMessageReceiver messageReceiver = null;
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        try {
+            messageReceiver = getMessageReceiver();
+            log.info("MessageReceiver is: " + messageReceiver.toString());
+            int completedCount = 0;
+            IMessage message = messageReceiver.receive();
+            log.info("received first message from DLQ: " + message.getMessageBody().toString());
+            while (message != null) {
+                /*if (canBeCompleted(message)) {
+                    logMessage(message);*/
+                log.info("Message is : " + mapper.readValue(message.getMessageBody().getBinaryData().get(0), String.class));
+                log.info("Message logging complete");
+                    messageReceiver.complete(message.getLockToken());
+                    completedCount++;
+                    log.info(
+                            "Completed message from envelopes dlq. messageId: {} Current time: {}",
+                            message.getMessageId(),
+                            Instant.now()
+                    );
+               /* } else {
+                    // just continue, lock on the current msg will expire automatically
+                    log.info("Leaving message on dlq, ttl has not passed yet. Message id: {}", message.getMessageId());
+                }*/
+                message = messageReceiver.receive();
+                //temporary break
+                break;
+            }
+
+            log.info("Finished processing messages in envelopes dlq. Completed {} messages", completedCount);
+        } catch (Exception e) {
+            log.error("Unable to connect to envelopes dead letter queue", e);
+        } finally {
+            if (messageReceiver != null) {
+                try {
+                    log.info("Closing the messageReceiver");
+                    messageReceiver.close();
+                } catch (ServiceBusException e) {
+                    log.error("Error closing dlq connection", e);
+                }
+            }
+        }
+        log.info("Finished {} job", "DlqProcessing");
+        System.exit(0);
+    }
+
+
+    public static IMessageReceiver getMessageReceiver() throws InterruptedException, ServiceBusException {
+        String connectionString = "Endpoint=sb://rd-servicebus-sandbox.servicebus.windows.net/;SharedAccessKeyName=SendAndListenSharedAccessKey;SharedAccessKey=97E6uvE6xHcqHAVlxufN1PH75tMHoZUe78FhsCbLLLQ=";
+
+        try {
+            return ClientFactory.createMessageReceiverFromConnectionStringBuilder(new ConnectionStringBuilder
+                    (connectionString, "rd-caseworker-topic-sandbox/subscriptions/temporary/$deadletterqueue"), ReceiveMode.PEEKLOCK);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.info("Interrupted");
+            throw e;
+        } catch (ServiceBusException e) {
+            log.info("ServiceBus exception" + e.getMessage());
+            throw e;
+        }
+    }
 
 }
